716e61976448e841cc641f08b13ced4d68356f91
Siraj Raval <sirajraval1@gmail.com>
2016-05-03 16:39:36 -0700

    convert step added

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import train
 if __name__ == '__main__':
     # service.py executed as script
     # do something
-    
+    execfile("convert_directory.py")
     execfile("train.py")
     execfile("generate.py")
