c353f96b5888dc181e4a8ab3d313a453e528edad
Siraj Raval <sirajraval1@gmail.com>
2016-05-03 15:55:04 -0700

    call it

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print "This line will be printed."
+import train
 
+
+if __name__ == '__main__':
+    # service.py executed as script
+    # do something
+    
+    execfile("train.py")
+    execfile("generate.py")
